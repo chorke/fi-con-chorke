1de87056a55daa1aaf57ab3e9296dbfe91ccb80a
chorke <jura.durani@gamil.com>
2014-08-22 17:30:49 +0200

    init project

---
+++ ficon/ficon-gui/src/main/java/org/chorke/ficon/gui/Main.java
@@ -0,0 +1,13 @@
+package org.chorke.ficon.gui;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
